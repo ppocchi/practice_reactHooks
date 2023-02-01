ebdf13cffbc927f38a845e0104358d473b6013bd
ajin <aj5210@naver.com>
2023-02-01 23:38:22 +0900

    useState ex2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import { useState } from 'react';
 
-function App() {
-    const [time, setTime] = useState(1);
+const initalVale = () => {
+    return ['홍길동'];
+}
 
-    const handleClick = () => {
-        let newTime;
-        if(12 <= time) {
-            newTime = 1;
-        }
-        else {
-            newTime = time + 1;
-        }
-        setTime(newTime);
+function App() {
+    const [names, setNames] = useState(() => {
+        return initalVale();
+    });
+    const [input, setInput] = useState('');
+
+    const handleInputChange = (e) => {
+        setInput(e.target.value);
+    };
+
+    const handleUpload = () => {
+        setNames((prevState) => {
+            return [input, ...prevState];
+        });
     };
 
     return (
         <div>
-            <span>시간: {time}시</span>
-            <button onClick={handleClick}>Update</button>
+            <input type="text" value={input} onChange={handleInputChange} />
+            <button onClick={handleUpload}>Upload</button>
+            {names.map((name, idx)=> {
+                return <p key={idx}>{name}</p>;
+            })}
         </div>
     );
 }
