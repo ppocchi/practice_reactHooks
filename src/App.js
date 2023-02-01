d3a9dac50aeadc5d653d8fc5c98ac4ce6463b913
ajin <aj5210@naver.com>
2023-02-02 00:52:10 +0900

    useRef ex2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import { useState, useRef, useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 function App() {
-    const [count, setCount] = useState(1);
-    const renderCount = useRef(0);
+    const inputRef = useRef();
 
     useEffect(() => {
-        renderCount.current += 1;
-        console.log('렌더링 수: ', renderCount.current);
-    });
+        inputRef.current.focus();
+    }, []);
+
+    const login = () => {
+        alert(`환영합니다 ${inputRef.current.value}`);
+        inputRef.current.focus();
+    };
 
     return (
-        <div>
-            <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>증가</button>
-        </div>
+       <div>
+           <input ref={inputRef} type="text" placeholder="username"></input>
+           <button onClick={login}>로그인</button>
+       </div>
     );
 };
 
